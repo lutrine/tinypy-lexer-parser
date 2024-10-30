ac03793a2ac10a2c30e55fd0087a848c07591c18
Caden Jamason <me@lutrine.dev>
2024-10-30 07:54:18 -0700

    fix(gui): :fire: replace accidental duplication of code from lexer
    Accidentally pasted the same code for `lexer.py` into `gui.py`. This commit fixes that dumb mistake.

--- gui.py
+++ gui.py
@@ -1,89 +1,72 @@
-import re
+import tkinter as tk
+import lexer as lex
+
+line_number = 0
+code_lines = []
+code = ""
 
 
-def cut_one_line_tokens(line):
-    output = []
-    op = re.compile(r"[=+>*]")  # Operator regex formula
-    key = re.compile(r"(if|else|int|float)\b")  # Keyword regex formula
-    sep = re.compile(r'[():";]')  # Separator regex formula
-    str_lit = re.compile(
-        r'"'
-    )  # String_Literal regex formula (i know it looks wrong, just trust)
-    iden = re.compile(r"([A-Za-z])([A-Za-z0-9]+)?")  # Identifier regex formula
-    flo = re.compile(
-        r"(?<![\d.A-Za-z])-?[0-9]+?\.[0-9]+\b(?![\d.A-Za-z])"
-    )  # Float_literal regex formula
-    int_lit = re.compile(
-        r"(?<![\d.A-Za-z])-?\d+\b(?![\d.A-Za-z])"
-    )  # Int_Literal regex formula
+def next_line():
+    global line_number, code_lines, code
+    output_text.focus_set()
+    output_text.config(state=tk.NORMAL)
 
-    while True:
-        if line == "":
-            break
-        if op.match(line):  # check for operators
-            lex = op.match(line)
-            tup = "<Operator," + lex.group(0) + ">"
-            output.append(tup)
-            line = line[lex.end() :]
+    if line_number == len(code_lines) - 1:
+        # If we have reached the end of the code lines, reset the line number
+        line_number = 0
+        code_lines = []
+        output_text.config(state=tk.NORMAL)
+        output_text.delete("1.0", tk.END)
 
-        elif key.match(line):  # check for Identifiers
-            lex = key.match(line)
-            tup = (
-                "<Keyword," + lex.group(0) + ">"
-            )  # [match object].group(0) is the actual thing the regex formula found in its entirety
-            output.append(tup)
-            line = line[lex.end() :]
+    if not code_lines:
+        code = code_text.get("1.0", tk.END)
+        code_lines = code.split("\n")
 
-        elif sep.match(line):  # check for separators
-            lex = sep.match(line)
-            tup = "<Separator," + lex.group(0) + ">"
-            output.append(tup)
-            line = line[lex.end() :]
-            if lex.group(0) == '"':
-                if str_lit.search(
-                    line
-                ):  # if a " separator is found, look for another " and all characters inbetween are a String_Literal
-                    relex = str_lit.search(line)
-                    retup = "<String_Literal," + line[: relex.start()] + ">"
-                    output.append(retup)
-                    reretup = "<Separator," + relex.group(0) + ">"
-                    output.append(reretup)
-                    line = line[relex.end() :]
+    output_text.insert(tk.END, f"{lex.cut_one_line_tokens(code_lines[line_number])}\n")
+    output_text.config(state=tk.DISABLED)
 
-        elif iden.match(line):  # check for Identifier
-            lex = iden.match(line)
-            tup = "<Identifier," + lex.group(0) + ">"
-            output.append(tup)
-            line = line[lex.end() :]
-
-        elif flo.match(line):  # check for Float_Literal
-            lex = flo.match(line)
-            tup = "<Float_Literal," + lex.group(0) + ">"
-            output.append(tup)
-            line = line[lex.end() :]
-
-        elif int_lit.match(line):  # look for Int_Literal
-            lex = int_lit.match(line)
-            tup = "<Int_Literal," + lex.group(0) + ">"
-            output.append(tup)
-            line = line[lex.end() :]
-
-        else:
-            line = line[1:]
-
-    return output
+    line_number += 1
+    line_number_text.config(state=tk.NORMAL)
+    line_number_text.delete("1.0", tk.END)
+    line_number_text.insert(tk.END, f"{line_number}")
+    line_number_text.config(state=tk.DISABLED)
 
 
-if __name__ == "__main__":
-    inputs = [
-        "int  A1=5",
-        "float BBB2     =1034.2",
-        "float     cresult     =     A1     +BBB2     *      BBB2",
-        'print("TinyPie")',
-    ]
+root = tk.Tk()
+root.title("Lexical Analyzer for TinyPie")
+root.bind("<Escape>", lambda e: root.quit())
 
-    for x in inputs:
-        print("Test Input String: " + x)
-        out = cut_one_line_tokens(x)
-        print("Output <Type,Token> list: ", end="")
-        print(out)
+# Code frame
+code_frame = tk.Frame(root)
+code_frame.pack(side="left")  # Set side to "left"
+
+code_label = tk.Label(code_frame, text="Source Code Here:")
+code_label.pack()
+
+code_text = tk.Text(code_frame, height=10, width=50)
+code_text.pack()
+
+run_button = tk.Button(code_frame, text="Next line", command=next_line)
+run_button.pack(side="left")
+
+line_number_label = tk.Label(code_frame, text=f"Line number")
+line_number_label.pack(side="right")
+
+line_number_text = tk.Text(code_frame, height=1, width=5)
+line_number_text.pack(side="right")
+
+# Output frame
+output_frame = tk.Frame(root)
+output_frame.pack(side="right")  # Set side to "right"
+
+output_label = tk.Label(output_frame, text="Output:")
+output_label.pack()
+
+output_text = tk.Text(output_frame, height=10, width=50)
+output_text.pack()
+output_text.config(state=tk.DISABLED)  # Set initial state to disabled
+
+quit_button = tk.Button(output_frame, text="Quit", command=root.quit)
+quit_button.pack(side="right")
+
+root.mainloop()
